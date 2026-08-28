c8468005f19889516972400647f88ac86720c95a
CleversonCSousa <cleversonsousa.git@gmail.com>
2026-08-28 14:55:51 -0300

    fix(technician): add validations for the fields to be updated

--- src/main/java/com/cleverson/help_desk/technician/presentation/UpdateTechnicianRequestDTO.java
+++ src/main/java/com/cleverson/help_desk/technician/presentation/UpdateTechnicianRequestDTO.java
@@ -1,14 +1,24 @@
 package com.cleverson.help_desk.technician.presentation;
 
 import com.cleverson.help_desk.technician.domain.WorkingHour;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 import java.util.UUID;
 
 public record UpdateTechnicianRequestDTO(
+        @NotNull(message = "Id is required")
         UUID id,
+
+        @NotBlank(message = "Name is required")
         String name,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
         String email,
+
         List<WorkingHour>workingHours
 ) {
 }
