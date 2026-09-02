49d8551df49afcb6b3211691d6f37f0f79eea1ed
CleversonCSousa <cleversonsousa.git@gmail.com>
2026-09-02 16:45:12 -0300

    fix(security): remove leftover merge conflict markers

--- src/main/java/com/cleverson/help_desk/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/cleverson/help_desk/infrastructure/security/SecurityConfig.java
@@ -69,8 +69,6 @@ public class SecurityConfig {
 
                         .requestMatchers(HttpMethod.PUT, "/technicians", "/technicians/**").hasRole(UserRole.ADMIN.name())
 
-<<<<<<< Updated upstream
-=======
                         // protects the child routes of /technicians (POST method)
 
                         .requestMatchers(HttpMethod.POST, "/technicians").hasRole(UserRole.ADMIN.name())
@@ -78,7 +76,6 @@ public class SecurityConfig {
                         // protects the /customers route (POST method)
                         .requestMatchers(HttpMethod.POST, "/tickets").hasAnyRole(UserRole.ADMIN.name(), UserRole.CUSTOMER.name())
 
->>>>>>> Stashed changes
                         .anyRequest().authenticated()
                 )
 
